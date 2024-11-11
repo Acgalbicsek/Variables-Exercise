e761192392dac62211bbb5ef50c2064b4180ccee
Amy Galbicsek <acgalbicsek@gmail.com>
2024-11-11 14:42:59 -0900

    updated work

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string catName = "Rocky";
+            int catAge = 5;
+            char sex = 'M';
+            bool isDeclawed = true;
+            double weight = 20.5;
+            decimal dailyTreatConsumption = 8.754m;
+
+
+
+        Console.WriteLine($"My fat cat's name is {catName}, he weighs {weight} pounds.);
+
+
         }
     }
 }
